3a8a5efd8135a699940ef3879b3738079121004b
corenitnmace <corentin.mace@ynov.com>
2019-12-26 14:32:07 +0100

    init

---
+++ src/app/title-container/title-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-title-container',
+  templateUrl: './title-container.component.html',
+  styleUrls: ['./title-container.component.scss']
+})
+export class TitleContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
